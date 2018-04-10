54ff61cf09ba23b225830b9222615e7a39ae62ec
Mike Larocca <michaellarocca90@gmail.com>
2018-04-09 17:04:11 -0700

    troubleshooting api

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,6 +4,7 @@ import '../css/Register.css';
 import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 const env = runtimeEnv()
+const url = env.REACT_APP_API_URL
 
 
 class Register extends Component {
@@ -55,6 +56,7 @@ handleChange(e){
 
 newUserSubmit(event){
   console.log(this.state.apiURL);
+  console.log(url);
   //when a submission happens we are NOT sending a url with parameters, opting to send json state object instead
   event.preventDefault()
   //set newUser to state
